6a05481fed4701badba4b3c16551ccdf30c2dc1f
mahisingh06330-arch <mahisingh06330@gmail.com>
2026-01-10 23:38:10 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Header, Footer, Possibility, Blog, WhatGPT3, Features} from './containers'
+import{Navbar, Brand, CTA} from './components'
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-        <h1>First</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+       <Brand/>
+       <WhatGPT3/>
+       <Features/>
+       <Possibility/>
+       <CTA/>
+       <Blog/>
+       <Footer/>
     </div>
   )
 }
